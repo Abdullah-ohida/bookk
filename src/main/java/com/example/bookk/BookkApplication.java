43fa142c5a5ca71eb8db86891bc0d3c79433a681
whalewalker <mongodbms@gmail.com>
2021-06-21 12:02:37 +0100

    E-book library project setup

---
+++ src/main/java/com/example/bookk/BookkApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookk/BookkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
